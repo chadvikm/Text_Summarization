98339fe48a6d5734b33a727e07f2265e9a9397ca
chadvikm <chadvik@outlook.com>
2024-04-09 17:05:54 -0700

    requirements updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+with open("README"), "r", encoding="utf-8") as f:
+    long_descriptin = f.read()
+
+Repo_name="Text_Summarization"
+author_username="chadvikm"
+src_repo="textSummarizer"
+author_email="chadvik@outlook.com"
